1cbe6d6fd0c07967af32db03e549aa660b769301
Falcon-51 <92328230+Falcon-51@users.noreply.github.com>
2021-12-16 16:23:59 +0300

    uploaded code files
    Загрузил главный файл.
    Создал класс Menu
    Реализовал стартовый интерфейс и работу с ним

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+﻿#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <list>
+#include "Menu.h"
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+	setlocale(0, "rus");
+    RenderWindow app(VideoMode(W, H), "Aquarium", Style::Close | Style::Titlebar);
+    
+    Menu menu;
+    
+
+    while (app.isOpen()) //Пока окно открыто
+    {
+        menu.interact(app);
+        Event event;
+        while (app.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                app.close();
+        }
+        
+    }
+	return 0;
+}
