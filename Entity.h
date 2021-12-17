687fbc78da4d5a2ac7298f42bb632c46ac60dc79
Falcon-51 <92328230+Falcon-51@users.noreply.github.com>
2021-12-18 00:23:02 +0300

    uploaded class "Entity" files
    Создал класс "Entity"
    Отредактировал главный файл

---
+++ Entity.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Animation.h"
+
+class Entity
+{
+public:
+    float x, y, dx, dy, R, angle;
+    bool life;
+    string name;
+    Animation anim;
+
+    Entity();
+ 
+
+    void settings(Animation& a, int X, int Y, float Angle = 0, int radius = 1);
+   
+
+    virtual void update() {};
+
+    void draw(RenderWindow& app);
+   
+
+    virtual ~Entity() {};
+};
